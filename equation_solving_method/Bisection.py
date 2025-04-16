4633e9308e828fbfaa67c9afdb1d16bd95f5b6fd
DoanVinhNhan <155894621+DoanVinhNhan@users.noreply.github.com>
2025-04-16 16:55:58 +0700

    Update Bisection.py
    Cập nhật thêm cách đánh giá sai số tuyệt đối, sai số tương đối và cho trước số lần lặp cho phương pháp Chia đôi

--- equation_solving_method/Bisection.py
+++ equation_solving_method/Bisection.py
@@ -2,48 +2,39 @@ from math import *
 from sympy import *
 import pandas as pd
 class bisection_oop:
-    def __init__(self, a_0, b_0, eps, expr):
-        x = symbols("x")
-        expr = lambdify(x, expr, "numpy")
+    def __init__(self, a_0, b_0,option, option_num, expr):
         self.f = expr
         self.a_0 = a_0
         self.b_0 = b_0
-        self.eps = eps
+        self.option = option
+        self.option_num = option_num
 
-    def __checkInputValidity(self):
-        L = self.a_0
-        R = self.b_0
-        # Check if a < b
-        if(L > R or (L == R and self.f(L) != 0)): return 0
-        # Check if f(a) * f(b) < 0
-        if(self.f(L) * self.f(R) >= 0): return 0
-        return 1
-
-    def __bisectionMethod(self):
+    def __bisectionEpsilonMethod(self):
         nIterations = 0
         left    = self.a_0
         right   = self.b_0
-        epsilon = self.eps
-        df = pd.DataFrame(columns = ["left", "right", "mid", "left sign" "delta%"])
+        epsilon = self.option_num
+        df = pd.DataFrame(columns = ["left", "right", "mid", "left sign" "Absolute error"])
 
         # Special case: f(a) = 0 or f(b) = 0
-        if(self.f(left) == 0): return pd.DataFrame([{"left": left, "right": right, "solution": left}])
-        if(self.f(right) == 0): return pd.DataFrame([{"left": left, "right": right, "solution": right}])
+        if(self.f(left) == 0): return pd.DataFrame([{"left": left, "right": right, "solution": left}]), 0, left
+        if(self.f(right) == 0): return pd.DataFrame([{"left": left, "right": right, "solution": right}]), 0, right
             
         # Evaluation phase
         mid_prev = left
         mid = (left + right) / 2
         lft_sign = 1 if self.f(left) >= 0 else -1
-        while abs(mid - mid_prev)/abs(mid) >= epsilon/100:
+        while abs(mid - mid_prev) >= epsilon:
             
             if (nIterations !=0): mid_prev = mid
 
             mid = (left + right) / 2            
-            new_row = pd.DataFrame([{"left": left, "right": right, "mid": mid, "sign":sign(self.f(mid)), "delta%": abs(mid - left)/abs(mid)}])
+            new_row = pd.DataFrame([{"left": left, "right": right, "mid": mid, "sign":sign(self.f(mid)),
+                                     "Absolute error": abs(mid - mid_prev)}])
             if df.empty: df = new_row
             else: df = pd.concat([df, new_row], ignore_index=True)
             val = self.f(mid)
-            if(val == 0): return df
+            if(val == 0): return df, nIterations, mid
             if(val * lft_sign < 0):
                 right = mid
             else:
@@ -51,6 +42,74 @@ class bisection_oop:
             nIterations = nIterations + 1
         result = mid
         return df, nIterations, result
+        
+    def __bisectionDeltaMethod(self):
+        nIterations = 0
+        left    = self.a_0
+        right   = self.b_0
+        delta = self.option_num
+        df = pd.DataFrame(columns = ["left", "right", "mid", "left sign" "Relative error"])
 
+        # Special case: f(a) = 0 or f(b) = 0
+        if(self.f(left) == 0): return pd.DataFrame([{"left": left, "right": right, "solution": left}]), 0, left
+        if(self.f(right) == 0): return pd.DataFrame([{"left": left, "right": right, "solution": right}]), 0, right
+            
+        # Evaluation phase
+        mid_prev = left
+        mid = (left + right) / 2
+        lft_sign = 1 if self.f(left) >= 0 else -1
+        check = abs(mid - mid_prev)/abs(mid) if mid!=0 else delta+1
+        while check >= delta:
+            
+            if (nIterations !=0): mid_prev = mid
+
+            mid = (left + right) / 2            
+            new_row = pd.DataFrame([{"left": left, "right": right, "mid": mid, "sign":sign(self.f(mid)),
+                                     "Relative error": abs(mid - mid_prev)/abs(mid) if mid!=0 else "NaN"}])
+            if df.empty: df = new_row
+            else: df = pd.concat([df, new_row], ignore_index=True)
+            val = self.f(mid)
+            if(val == 0): return df, nIterations, mid
+            if(val * lft_sign < 0):
+                right = mid
+            else:
+                left = mid
+            nIterations = nIterations + 1
+            check = abs(mid - mid_prev)/abs(mid) if mid!=0 else delta+1
+        result = mid
+        return df, nIterations, result
+        
+    def __bisectionNumMethod(self):
+        left    = self.a_0
+        right   = self.b_0
+        n = self.option_num
+        df = pd.DataFrame(columns = ["left", "right", "mid", "left sign" "Absolute error"])
+        mid = left
+        lft_sign = 1 if self.f(left) >= 0 else -1
+        if(self.f(left) == 0): 
+            return pd.DataFrame([{"left": left, "right": right, "solution": left}]), 0, left
+        if(self.f(right) == 0): 
+            return pd.DataFrame([{"left": left, "right": right, "solution": right}]), 0, right
+        for i in range(n):
+            mid_prev = mid
+            mid = (left+right)/2
+            new_row = pd.DataFrame([{"left": left, "right": right, "mid": mid, "sign":sign(self.f(mid)),
+                                     "Absolute error": abs(mid - mid_prev)}])
+            if df.empty: df = new_row
+            else: df = pd.concat([df, new_row], ignore_index=True)
+            val = self.f(mid)
+            if(val == 0): return df, i, mid
+            if(val * lft_sign < 0):
+                right = mid
+            else:
+                left = mid
+        result = mid
+        return df, n, result    
     def Solve(self):
-        return self.__bisectionMethod()
+        option = self.option
+        if option == "Sai số tuyệt đối":
+            return self.__bisectionEpsilonMethod()
+        elif option == "Sai số tương đối":
+            return self.__bisectionDeltaMethod()
+        else:
+            return self.__bisectionNumMethod()

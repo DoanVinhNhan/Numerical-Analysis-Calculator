09fffc0a2f6f04bd4946a7b5fcc0f903483890d5
DoanVinhNhan <155894621+DoanVinhNhan@users.noreply.github.com>
2025-04-21 14:38:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import customtkinter as ctk
 from sympy import *
 import pandas as pd
 from frame_lib import *
-from PIL import Image
-from equationMethods import bisection_oop, secant_oop
+from equationMethods import bisection_oop, secant_oop, newton1_oop
 
 root = ctk.CTk()
 root.title("Numerical Analysis Calculator")
@@ -20,6 +19,7 @@ nonlinear_system_frame_instance = nonlinear_system_frame(root, frame_manager)
 matrix_frame_instance = matrix_frame(root, frame_manager)
 bisection_frame_instance = bisection_frame(root, frame_manager)
 secant_frame_instance = secant_frame(root, frame_manager)
+newton1_frame_instance = newton1_frame(root, frame_manager)
 
 frame_manager.add_frame("main_frame", main_frame_instance)
 frame_manager.add_frame("equation_frame", equation_frame_instance)
@@ -27,7 +27,8 @@ frame_manager.add_frame("nonlinear_system_frame",nonlinear_system_frame_instance
 frame_manager.add_frame("matrix_frame",matrix_frame_instance)
 frame_manager.add_frame("bisection_frame",bisection_frame_instance)
 frame_manager.add_frame("secant_frame",secant_frame_instance)
+frame_manager.add_frame("newton1_frame",newton1_frame_instance)
 
-frame_manager.switch_frame("secant_frame")
+frame_manager.switch_frame("newton1_frame")
 
 root.mainloop()

0f91f1e137520ea759a95f0c7da317f07e29210c
DoanVinhNhan <155894621+DoanVinhNhan@users.noreply.github.com>
2025-04-26 23:56:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tkinter as tk
-from tkinter import ttk
 import customtkinter as ctk
 from sympy import *
-import pandas as pd
 from frame_lib import *
 
 root = ctk.CTk()
@@ -20,7 +17,8 @@ bisection_frame_instance = bisection_frame(root, frame_manager)
 secant_frame_instance = secant_frame(root, frame_manager)
 newton1_frame_instance = newton1_frame(root, frame_manager)
 single_loop1_frame_instance = single_loop1_frame(root, frame_manager)
-newton_raphson_frame_instance = newton_raphson_frame(root, frame_manager)
+newton_raphson_frame_instance = newton_raphson_frame(root,frame_manager)
+single_loop_frame_instance = single_loop_frame(root, frame_manager)
 
 frame_manager.add_frame("main_frame", main_frame_instance)
 frame_manager.add_frame("equation_frame", equation_frame_instance)
@@ -30,7 +28,8 @@ frame_manager.add_frame("bisection_frame",bisection_frame_instance)
 frame_manager.add_frame("secant_frame",secant_frame_instance)
 frame_manager.add_frame("newton1_frame",newton1_frame_instance)
 frame_manager.add_frame("single_loop1_frame", single_loop1_frame_instance)
-frame_manager.add_frame("newton_raphson_frame", newton_raphson_frame_instance)
+frame_manager.add_frame("single_loop_frame", single_loop_frame_instance)
+frame_manager.add_frame("newton_raphson_frame",newton_raphson_frame_instance)
 
 frame_manager.switch_frame("newton_raphson_frame")
 
